7f803e355767be97b34945fb16312518849e1dcb
Bill Siever <bsiever@gmail.com>
2022-09-22 16:51:57 -0500

    Initial commit.

---
+++ speech.ts
@@ -0,0 +1,15 @@
+
+
+//% color=#0000FF 
+//% icon="\uf074"
+//% block="speech"
+//% group="micro:bit (V2)"
+namespace speech {
+    // //% blockId="test" block="test speech %string|"
+    // //% shim=speech::test
+    // //% group="micro:bit (V2)"
+    // export function test(string:  : string) : void {
+    //     // Per https://github.com/microsoft/pxt-microbit/issues/4292
+    //     0;
+    // }
+}
